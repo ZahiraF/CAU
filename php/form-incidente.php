449a72b2a34af133c202bd1b97dd416ae0d1d969
ZahiraF <fernandez.zahira.epet20@gmail.com>
2023-07-07 01:29:02 -0300

    editar formulario

---
+++ php/form-incidente.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$dni = $_POST['dni'];
+$email = $_POST['email-solicitante'];
+$asunto = $_POST['asunto'];
+$descripcion = $_POST['descripcion'];
+?>
